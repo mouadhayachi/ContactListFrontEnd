819da7f2f282f4493fb5b9806748916501894bda
mouadhayachi <mouadh.ayachii@gmail.com>
2020-05-04 16:03:12 +0100

    first commit

--- src/App.js
+++ src/App.js
@@ -1,26 +1,29 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import { Button } from "react-bootstrap";
+import { BrowserRouter as Router, Route, Link } from "react-router-dom";
+
+import ContactList from "./components/ContactList/ContactList";
+import AddContact from "./components/AddContact/AddContact";
+
+import "./App.css";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <div className="home-page">
+        <h1>Contact App</h1>
+        <div>
+          <Link to="/contactlist">
+            <Button className="btn btn-warning btn-home">Contact List</Button>
+          </Link>
+          <Link to="/addcontact">
+            <Button className="btn btn-primary btn-home">Add Contact</Button>
+          </Link>
+        </div>
+      </div>
+      <Route exact path="/addcontact" component={AddContact}></Route>
+      <Route exact path="/contactlist" component={ContactList} />
+    </Router>
   );
 }
-
 export default App;
